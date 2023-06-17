63bb567c9b6285c6514041be37b986cff470b51d
Gabe Dunn <gabe@sent.at>
2023-06-17 15:38:28 -0600

    feat: add utility functions

---
+++ src/utils.ts
@@ -0,0 +1,15 @@
+// utility functions
+
+// random number generator
+const random = (upTo: number): number => Math.floor(Math.random() * upTo) + 1
+
+// probability tester
+export const should = (outOf: number): boolean => random(outOf) === 1
+
+// capitalize the first letter
+export const upCase = (str: string): string =>
+	`${str[0].toUpperCase()}${str.slice(1)}`
+
+// select random element
+export const selectFrom = (arr: string[]): string =>
+	arr[Math.floor(Math.random() * arr.length)]
